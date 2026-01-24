c242ecfd4ac188e94bc62e386a6b4517ff5b7f71
shafiqahmad81 <shafiqahmad081@gmail.com>
2026-01-24 19:11:19 +0600

    update all page

--- src/main.js
+++ src/main.js
@@ -468,3 +468,42 @@ document.querySelectorAll(".accordion-wrap").forEach(wrap => {
 
   updateRange();
 
+
+  // Lightbox slider
+
+
+  $(document).ready(function() {
+$('.arch-slider').slick({
+slidesToShow: 3,
+slidesToScroll: 1,
+infinite: false,
+arrows: true,
+variableWidth: true,
+prevArrow: '<div class="prev"><svg width="23" height="39" viewBox="0 0 23 39"><path class="slideshow-arrow" d="M154.994,259.522L153.477,261l-18.471-18,18.473-18,1.519,1.48L138.044,243Z"></path></svg></div>',
+nextArrow: '<div class="next"><svg width="23" height="39" viewBox="0 0 23 39"><path class="slideshow-arrow" d="M857.005,231.479L858.5,230l18.124,18-18.127,18-1.49-1.48L873.638,248Z"></path></svg></div>',
+responsive: [
+{ breakpoint: 1000, settings: { slidesToShow: 2, slidesToScroll: 1 } },
+{ breakpoint: 650, settings: { slidesToShow: 1, slidesToScroll: 1 } }
+]
+});
+
+
+// Adjust each slide width to fit image
+function sliderResizer() {
+$('.slick-slide').each(function() {
+var w = 0;
+$(this).find('img').each(function() {
+if ($(this).width() > w) w = $(this).width();
+});
+$(this).css('width', w);
+});
+}
+$(window).on('load resize', sliderResizer);
+sliderResizer();
+
+
+// Enable Lightbox
+$('.arch-slider').slickLightbox({
+itemSelector: '.arc-link'
+});
+});
